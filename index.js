d3655fcc17a9fe881feba902b01b6396e34b332c
ulvi_199517 <ulvi199517@gmail.com>
2021-10-05 16:02:16 -0400

    implemented express router. So now we receive dishes route from another file and combine it in index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const morgan = require('morgan')
 
+const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -9,39 +10,10 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.all('/dishes', (req,res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
-app.get('/dishes', (req,res) => {
-    res.end('Will send all the dishes to you!');
-});
-app.post('/dishes', (req, res, next) => {
-    res.end('Will add the dish: ' + req.body.name + ' with details ' + req.body.description);
-});
-app.put('/dishes', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('Put operations not supported on /dishes');
-});
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all the dishes!');
-});
-app.get('/dishes/:dishId', (req,res) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-});
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.end('Post operations not supported on /dishes/' + req.params.dishId);
-});
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish:' + req.params.dishId);
-});
-
+app.use('/dishes', dishRouter);
 app.use(express.static(__dirname+'/public'));
+
+
 
 app.use( (req,res, next) => {
     
@@ -54,3 +26,26 @@ const server = http.createServer(app);
 server.listen(port, hostname, () => {
     console.log(`Server running at http:///${hostname}:${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+// app.get('/dishes/:dishId', (req,res) => {
+//     res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+// });
+// app.post('/dishes/:dishId', (req, res, next) => {
+//     res.end('Post operations not supported on /dishes/' + req.params.dishId);
+// });
+// app.put('/dishes/:dishId', (req, res, next) => {
+//     res.write('Updating the dish: ' + req.params.dishId + '\n');
+//     res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+// });
+// app.delete('/dishes/:dishId', (req, res, next) => {
+//     res.end('Deleting dish:' + req.params.dishId);
+// });
---
+++ routes/dishRouter.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const dishRouter = express.Router();
+
+dishRouter.use(express.json());
+
+dishRouter.route('/')
+.all((req,res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res) => {
+    res.end('Will send all the dishes to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Put operations not supported on /dishes');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all the dishes!');
+});
+module.exports = dishRouter;
